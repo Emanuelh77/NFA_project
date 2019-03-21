cc50e2ce9f3801afcbc53f99c15fb1a0cb70fcbf
rlowies <ronlowies@u.boisestate.edu>
2019-03-21 15:39:14 -0600

    Created required classes

---
+++ fa/nfa/NFA.java
@@ -0,0 +1,13 @@
+package fa.nfa;
+/*
+ * NFA class must implement fa.nfa.NFAInterface interface.
+ * Make sure to implement all methods inherited from those interfaces.
+ * You have to add instance variables representing NFA elements.
+ * You can also write additional methods which must be private,
+ * i.e., only helper methods.
+ *
+ * @author Ron Lowies
+ */
+public class NFA {
+
+}
---
+++ fa/nfa/NFAState.java
@@ -0,0 +1,10 @@
+package fa.nfa;
+/*
+ * NFAState class must extend fa.State abstract class.
+ * If your implementation requires it,
+ * you can add additional instance variables and methods to your NFAState class.
+ *
+ * @author Ron Lowies
+ */
+public class NFAState {
+}
