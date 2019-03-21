a711b977b98e6d899a17bb631185e1a59058d5c2
rlowies <ronlowies@u.boisestate.edu>
2019-03-21 17:21:28 -0600

    TODO: Implement NFA methods to construct states needed for transition table

--- fa/nfa/NFA.java
+++ fa/nfa/NFA.java
@@ -2,7 +2,10 @@ package fa.nfa;
 
 import fa.State;
 import fa.dfa.DFA;
+import fa.dfa.DFAState;
 
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Set;
 
 /*
@@ -18,10 +21,31 @@ public class NFA implements NFAInterface {
 
     private Set<NFAState> states,eClosure;
     private NFAState start;
+    private Queue<Set<NFAState>> queue;
 
     @Override
     public DFA getDFA() {
-
+        queue = new LinkedList<>();
+        queue.add(eClosure(start)); //Start the queue with the first state
+        DFA dfa = new DFA();        //Create a dfa
+        while(!queue.isEmpty()) {   //Iterate over queue elements
+           for(NFAState nState : queue.remove()) { //Each element is a set containing states
+                if(dfa.getStartState() == null) {  //Add start state
+                    dfa.addStartState(nState.getName());
+                }
+                dfa.addState(nState.getName());
+                /*TODO: Implement NFA methods to construct states needed for transition table
+                 * For example: if NFA has states S,M,F
+                 * has transitions: SaS SaM SbS MaM MaF MbM MbF
+                 * and E(S) = { S }
+                 * dfa states found through transistion table are
+                 * { S }
+                 * { S, M }
+                 * { S, M, F}
+                 * Need to be used to create the DFA.
+                 */
+           }
+        }
         return null;
     }
 
